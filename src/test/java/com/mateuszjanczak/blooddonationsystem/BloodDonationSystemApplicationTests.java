c6b1fb9cc95b66e5942c26b022fa8d02b2a94602
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-02-15 01:52:53 +0100

    Initial commit

---
+++ src/test/java/com/mateuszjanczak/blooddonationsystem/BloodDonationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszjanczak.blooddonationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
